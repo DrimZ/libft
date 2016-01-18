d4a1d583d96a01329a8fe496bab28c5751e171f3
Quentin LE BEVILLON <qle-bevi@student.42.fr>
2016-01-18 16:02:15 +0100

    normed

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: qle-bevi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/29 23:17:45 by qle-bevi          #+#    #+#             */
-/*   Updated: 2015/11/29 23:18:12 by qle-bevi         ###   ########.fr       */
+/*   Updated: 2016/01/18 16:01:28 by qle-bevi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,7 +105,7 @@ t_list			*ft_lstnew(void const *content, size_t content_size);
 void			ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstadd(t_list **alst, t_list *new);
-void			ft_lstpush(t_list **begin, void const *content, size_t content_size);
+void			ft_lstpush(t_list **begin, void const *content, size_t size);
 void			ft_lstpush2(t_list **begin, t_list *new);
 void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
