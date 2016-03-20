7ddd712935ecad210683f28a9c31b7bb5fee448b
Quentin LE BEVILLON <qle-bevi@e1r11p9.42.fr>
2016-03-20 17:20:28 +0100

    better free tab

--- ft_freetab.c
+++ ft_freetab.c
@@ -6,7 +6,7 @@
 /*   By: qle-bevi <qle-bevi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/18 12:58:35 by qle-bevi          #+#    #+#             */
-/*   Updated: 2016/03/19 19:38:04 by qle-bevi         ###   ########.fr       */
+/*   Updated: 2016/03/20 17:19:36 by qle-bevi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_freetab(void **tab)
 {
 	int i;
 
-	if (!tab)
+	if (!tab || !*tab)
 		return ;
 	i = 0;
 	while (tab[i])
